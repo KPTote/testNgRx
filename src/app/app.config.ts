b83a804e4b734bd90f15a7363cdd4509a06d0b5e
Kevyn Posadas <tote.kev@gmail.com>
2025-11-26 10:19:47 -0600

    prueba 2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 
@@ -20,13 +20,14 @@ export const appConfig: ApplicationConfig = {
     }),
     provideEffects([PlaylistEffects]),
 
-    provideStoreDevtools({
+    isDevMode()
+    ? provideStoreDevtools({
       maxAge: 25, // Retains last 25 states
       logOnly: false, // Restrict extension to log-only mode
       autoPause: true, // Pauses recording actions and state changes when the extension window is not open
       trace: false, // If set to true, will include stack trace for every dispatched action, so you can see where the action was dispatched from
       traceLimit: 75, // The maximum number of stack trace frames to be stored (not applicable for Firefox)
-    }),
+    }) : [],
     provideHttpClient()
 
 
